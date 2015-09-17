61e9d12774b4756a078f155673c9533956cb5075
Karin <promo@nirak.net>
2015-09-17 16:04:35 -0500

    still figuring out scss and stuff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,12 +6,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( bootstrap.min.js )
-Rails.application.config.assets.precompile += %w( main.js )
-Rails.application.config.assets.precompile += %w( modernizr.min.js )
+#Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+#Rails.application.config.assets.precompile += %w( main.js )
+#Rails.application.config.assets.precompile += %w( modernizr.min.js )
 # Rails.application.config.assets.precompile += %w( jquery.min.js )
 
 #Rails.application.config.assets.precompile += %w( bootstrap-theme.min.css )
 #Rails.application.config.assets.precompile += %w( bootstrap.min.css )
-Rails.application.config.assets.precompile += %w( bootstrap.css )
-Rails.application.config.assets.precompile += %w( style.css )
+#Rails.application.config.assets.precompile += %w( bootstrap.css )
+#Rails.application.config.assets.precompile += %w( style.css )
