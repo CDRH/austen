f1a72a4728aca058055a3289fa85050fda10e222
Karin <promo@nirak.net>
2015-09-18 09:46:00 -0500

    Lots o changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,13 +21,15 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
+  
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Do not fallback to assets pipeline if a precompiled asset is missed. http://stackoverflow.com/questions/18700219/rails-4-assets-not-loading-in-production
+config.assets.compile = true
+# config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
