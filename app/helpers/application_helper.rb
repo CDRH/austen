1321a44962980ef7aa212760f9bcb39f84095443
William Dewey <wdewey2@unl.edu>
2026-07-15 15:48:53 -0500

    Display both page title and site title as title
    fixes #79
    revert if this has already been done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def site_title
+    "Austen Said"
+  end
+
+  def full_title(page_title = nil)
+    if page_title.present?
+      "#{page_title} | #{site_title}"
+    else
+      site_title
+    end
+  end
 end
